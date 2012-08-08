e4eb43facd727917f09a35b1a12ae335294694df
Anthony Bargnesi <abargnesi@gmail.com>
2012-08-08 11:50:57 -0400

    corrected derby system property
    previously referencing the wrong class

--- cli/whistle.cmd
+++ cli/whistle.cmd
@@ -1,9 +1,9 @@
 @echo off
 
 if not defined JAVA_OPTS (
-    set JAVA_OPTS=-Xmx1024m -Dderby.stream.error.field=com.selventa.belframework.common.enums.DatabaseType.NULL_OUTPUT_STREAM
+    set JAVA_OPTS=-Xmx1024m -Dderby.stream.error.field=org.openbel.framework.common.enums.DatabaseType.NULL_OUTPUT_STREAM
 ) else (
-    set JAVA_OPTS=%JAVA_OPTS% -Dderby.stream.error.field=com.selventa.belframework.common.enums.DatabaseType.NULL_OUTPUT_STREAM
+    set JAVA_OPTS=%JAVA_OPTS% -Dderby.stream.error.field=org.openbel.framework.common.enums.DatabaseType.NULL_OUTPUT_STREAM
 )
 
 java %JAVA_OPTS% -jar whistle-${project.version}.jar %*
--- cli/whistle.sh
+++ cli/whistle.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ -z "${JAVA_OPTS}" ]; then
-    JAVA_OPTS="-Xmx1024m -Dderby.stream.error.field=com.selventa.belframework.common.enums.DatabaseType.NULL_OUTPUT_STREAM"
+    JAVA_OPTS="-Xmx1024m -Dderby.stream.error.field=org.openbel.framework.common.enums.DatabaseType.NULL_OUTPUT_STREAM"
 else
-    JAVA_OPTS="$JAVA_OPTS -Dderby.stream.error.field=com.selventa.belframework.common.enums.DatabaseType.NULL_OUTPUT_STREAM"
+    JAVA_OPTS="$JAVA_OPTS -Dderby.stream.error.field=org.openbel.framework.common.enums.DatabaseType.NULL_OUTPUT_STREAM"
 fi
 
 java $JAVA_OPTS -jar whistle-${project.version}.jar "$@"
